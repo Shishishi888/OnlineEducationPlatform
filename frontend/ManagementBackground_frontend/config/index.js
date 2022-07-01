004cb11b7895fe8208ad9570e9cb647179defbc5
ShiYang <Shishishi996@163.com>
2022-07-01 11:59:38 +0800

    项目前端页面框架介绍；前端页面框架登录问题

--- frontend/ManagementBackground_frontend/config/index.js
+++ frontend/ManagementBackground_frontend/config/index.js
@@ -22,7 +22,8 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    // useEslint: true,
+    useEslint: false,  // 代码格式整理插件，不建议安装
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
