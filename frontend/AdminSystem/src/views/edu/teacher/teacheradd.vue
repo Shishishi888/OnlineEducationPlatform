c6a331c654812380c6449b94dafb0c0f03fa5cc5
ShiYang <Shishishi996@163.com>
2022-07-03 21:09:00 +0800

    讲师管理前端-路由切换问题以及解决方法

--- frontend/AdminSystem/src/views/edu/teacher/teacheradd.vue
+++ frontend/AdminSystem/src/views/edu/teacher/teacheradd.vue
@@ -56,16 +56,29 @@ export default {
       addBtnDisabled: false, // 是否禁用保存按钮
     };
   },
-  created() {  // 在页面渲染之前执行
-    // 判断路径中是否有讲师的ID值
-    if(this.$route.params && this.$route.params.id) {
-      // 从路径获取讲师的ID值
-      const id = this.$route.params.id;
-      // 调用根据讲师ID值查询讲师信息的方法
-      this.getTeacherInfo(id);
+  created() {  // 在页面渲染之前执行；当多次路由跳转到同一个页面时，该方法只执行第一次
+    this.init();
+  },
+  watch: {  // 监听
+    $route(to, from) {  // 当路由发生变化时，该方法就会执行
+      this.init();
     }
   },
   methods: {
+    init() {
+      // 判断路径中是否有讲师的ID值
+      if(this.$route.params && this.$route.params.id) {  // 路径中有讲师的ID值，说明用户正在进行修改讲师操作
+        // 从路径获取讲师的ID值
+        const id = this.$route.params.id;
+        // 调用根据讲师ID值查询讲师信息的方法
+        this.getTeacherInfo(id);
+      }
+      else {  // 路径中没有讲师的ID值，说明用户正在进行添加讲师操作
+        // 清空表单
+        this.teacher = {}
+      }
+    },
+
     addOrUpdate() {
       // 判断当前的操作是添加讲师还是修改讲师信息：根据this.teacher中是否有id值进行判断
       if(!this.teacher.id) {
