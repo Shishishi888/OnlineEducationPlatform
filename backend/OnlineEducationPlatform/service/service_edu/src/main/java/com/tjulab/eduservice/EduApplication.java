ba07d4bf6ecd6915c25518961ee57d4921fd7625
ShiYang <Shishishi996@163.com>
2022-07-16 23:37:20 +0800

    技术点-Nacos安装与服务注册

--- backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/EduApplication.java
+++ backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/EduApplication.java
@@ -2,10 +2,12 @@ package com.tjulab.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.tjulab"})  // 扫描com.tjulab下所有的配置类并启动
+@EnableDiscoveryClient  // 注册nacos
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
