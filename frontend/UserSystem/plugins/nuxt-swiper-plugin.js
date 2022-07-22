4e95a52af73ee1fad3d9a32291d346706ca77fa2
ShiYang <Shishishi996@163.com>
2022-07-22 23:47:41 +0800

    整合用户注册页面和用户登录页面

--- frontend/UserSystem/plugins/nuxt-swiper-plugin.js
+++ frontend/UserSystem/plugins/nuxt-swiper-plugin.js
@@ -1,4 +1,7 @@
 import Vue from 'vue';
 import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr';
+import ElementUI from 'element-ui';  // element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'; // element-ui的css
 
+Vue.use(ElementUI);  // 使用ElementUI
 Vue.use(VueAwesomeSwiper);
