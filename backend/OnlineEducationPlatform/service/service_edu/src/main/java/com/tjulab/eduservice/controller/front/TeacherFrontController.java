ff3c90fd3e224cdc12a560d357cc129f6fd59829
ShiYang <Shishishi996@163.com>
2022-07-27 15:10:31 +0800

    讲师详情页（后端接口）

--- backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/controller/front/TeacherFrontController.java
+++ backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/controller/front/TeacherFrontController.java
@@ -1,20 +1,28 @@
 package com.tjulab.eduservice.controller.front;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tjulab.commonutils.R.R;
+import com.tjulab.eduservice.entity.EduCourse;
 import com.tjulab.eduservice.entity.EduTeacher;
+import com.tjulab.eduservice.service.EduCourseService;
 import com.tjulab.eduservice.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/eduservice/teacherfront")
 @CrossOrigin
 public class TeacherFrontController {
+    
     @Autowired
     private EduTeacherService eduTeacherService;
+
+    @Autowired
+    private EduCourseService eduCourseService;
 
     /**
      * 查询讲师（分页查询）
@@ -27,6 +35,19 @@ public class TeacherFrontController {
         Page<EduTeacher> teacherPage = new Page<>(page, limit);
         Map<String, Object> teacherPageMap = eduTeacherService.getFrontTeacherPageList(teacherPage);
         return R.ok().data(teacherPageMap);
+    }
+
+    @GetMapping("getFrontTeacherInfo/{teacherId}")
+    private R getFrontTeacherInfo(@PathVariable String teacherId) {
+        // 1. 根据讲师id查询讲师的基本信息
+        EduTeacher eduTeacher = eduTeacherService.getById(teacherId);
+
+        // 2. 根据讲师id查询讲师所授的课程
+        QueryWrapper<EduCourse> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("teacher_id", teacherId);
+        List<EduCourse> eduCourseList = eduCourseService.list(queryWrapper);
+
+        return R.ok().data("teacherInfo", eduTeacher).data("courseList", eduCourseList);
     }
 
 }
