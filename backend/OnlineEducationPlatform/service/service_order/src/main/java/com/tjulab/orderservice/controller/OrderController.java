f26d490404e1d8391c7d80a1601eeec3fe04f733
ShiYang <Shishishi996@163.com>
2022-08-20 22:50:12 +0800

    课程支付功能-查询课程订单接口

--- backend/OnlineEducationPlatform/service/service_order/src/main/java/com/tjulab/orderservice/controller/OrderController.java
+++ backend/OnlineEducationPlatform/service/service_order/src/main/java/com/tjulab/orderservice/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.tjulab.orderservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.tjulab.commonutils.R.R;
 import com.tjulab.commonutils.jwt.JwtUtils;
+import com.tjulab.orderservice.entity.Order;
 import com.tjulab.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -40,5 +42,17 @@ public class OrderController {
         return R.ok().data("orderNo", orderNo);
     }
 
+    /**
+     * 查询课程订单信息（根据课程订单ID查询）
+     * @param orderId
+     * @return
+     */
+    @GetMapping("getOrderInfo/{orderId}")
+    public R getOrderInfo(@PathVariable String orderId) {
+        QueryWrapper<Order> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("order_no", orderId);
+        Order order = orderService.getOne(queryWrapper);
+        return R.ok().data("order", order);
+    }
 }
 
