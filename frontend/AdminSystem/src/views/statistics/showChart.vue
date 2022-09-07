37a41017855f9a38fcf31787db1af9cc93560f96
ShiYang <Shishishi996@163.com>
2022-09-07 09:13:54 +0800

    统计分析-图表显示-前端完善

--- frontend/AdminSystem/src/views/statistics/showChart.vue
+++ frontend/AdminSystem/src/views/statistics/showChart.vue
@@ -73,58 +73,61 @@ export default {
 
       // 指定图表的配置项和数据
       var option = {
-          title: {
-              text: '数据统计'
-          },
-          tooltip: {
-              trigger: 'axis'
-          },
-          dataZoom: [{
-              show: true,
-              height: 30,
-              xAxisIndex: [
-                  0
-              ],
-              bottom: 30,
-              start: 10,
-              end: 80,
-              handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
-              handleSize: '110%',
-              handleStyle: {
-                  color: '#d3dee5'
+        // 图表标题
+        title: {
+            text: '数据统计'
+        },
+        // x坐标轴触发提示
+        tooltip: {
+            trigger: 'axis'
+        },
+        // 区域缩放
+        dataZoom: [{
+            show: true,
+            height: 30,
+            xAxisIndex: [
+                0
+            ],
+            bottom: 30,
+            start: 10,
+            end: 80,
+            handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+            handleSize: '110%',
+            handleStyle: {
+                color: '#d3dee5'
 
-              },
-              textStyle: {
-                  color: '#fff'
-              },
-              borderColor: '#90979c'
-              },
-              {
-              type: 'inside',
-              show: true,
-              height: 15,
-              start: 1,
-              end: 35
-            }],
-          // x轴是类目轴（离散数据）,必须通过data设置类目数据
-          xAxis: {
-              type: 'category',
-              data: this.xData
-          },
-          // y轴是数据轴（连续数据）
-          yAxis: {
-              type: 'value'
-          },
-          // 系列列表。每个系列通过 type 决定自己的图表类型
-          series: [{
-              // 折线图
-              type: 'line',
-              // 系列中的数据内容数组
-              data: this.yData
-          }]
+            },
+            textStyle: {
+                color: '#fff'
+            },
+            borderColor: '#90979c'
+            },
+            {
+            type: 'inside',
+            show: true,
+            height: 15,
+            start: 1,
+            end: 35
+          }],
+        // x轴是类目轴（离散数据）,必须通过data设置类目数据
+        xAxis: {
+            type: 'category',
+            data: this.xData
+        },
+        // y轴是数据轴（连续数据）
+        yAxis: {
+            type: 'value'
+        },
+        // 系列列表。每个系列通过 type 决定自己的图表类型
+        series: [{
+            // 折线图
+            type: 'line',
+            // 系列中的数据内容数组
+            data: this.yData
+        }]
       }
 
-      this.chart.setOption(option)
+      this.chart.setOption(option);
     }
   }
 }
