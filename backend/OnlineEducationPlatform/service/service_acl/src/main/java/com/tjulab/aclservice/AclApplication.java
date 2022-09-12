2555214857be381f441ebbe2c4db86ff286a24ea
ShiYang <Shishishi996@163.com>
2022-09-12 22:59:37 +0800

    权限管理-整合后端接口

---
+++ backend/OnlineEducationPlatform/service/service_acl/src/main/java/com/tjulab/aclservice/AclApplication.java
@@ -0,0 +1,17 @@
+package com.tjulab.aclservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan("com.tjulab")
+@MapperScan("com.tjulab.aclservice.mapper")
+public class AclApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AclApplication.class, args);
+    }
+}
---
+++ backend/OnlineEducationPlatform/service/service_acl/src/main/java/com/tjulab/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tjulab.aclservice.service.impl;
+
+import com.tjulab.aclservice.entity.UserRole;
+import com.tjulab.aclservice.mapper.UserRoleMapper;
+import com.tjulab.aclservice.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
